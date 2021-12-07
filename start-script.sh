b2d179142bd63cfc80fc64b1a955d6e3afec4e8d
Sam Kerridge <sammykerridge@gmail.com>
2021-12-07 15:52:47 +1300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,7 +1,7 @@
 yum -y install git
 
-firewall-cmd add-port=25565/tcp --permanent
-firewall-cmd add-port=25565/tcp --permanent
+firewall-cmd --add-port=25565/tcp --permanent
+firewall-cmd --add-port=25565/udp --permanent
 
 firewall-cmd --reload
 
