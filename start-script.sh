493b36a4a5172090778f72c3dd40f136958c1db7
Sam Kerridge <sammykerridge@gmail.com>
2021-12-08 13:56:00 +1300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,7 +1,11 @@
 yum -y install git
 
 firewall-cmd --add-port=25565/tcp --permanent
-firewall-cmd --add-port=25565/udp --permanent
 
 firewall-cmd --reload
 
+docker-compose -f minecraft.yml up --detach vanilla
+
+docker-compose -f minecraft.yml down mc-server_vanilla_1
+
+docker logs --follow mc-server_vanilla_1
