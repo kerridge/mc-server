f9f188cb7cbe39d884f6c00dcf764766a04ebafd
Sam Kerridge <sam.kerridge@xero.com>
2022-04-15 16:27:01 +1200

    wack

--- start-script.sh
+++ start-script.sh
@@ -9,3 +9,14 @@ docker-compose -f minecraft.yml up --detach vanilla
 docker-compose -f minecraft.yml down mc-server_vanilla_1
 
 docker logs --follow mc-server_vanilla_1
+
+
+alias rcon='docker exec -i mc-server_"$1"_1 rcon-cli'
+function logs {
+    type logs
+    docker logs -f mc-server_$1\_1
+}
+alias down='docker-compose -f minecraft.yml down'
+alias up='docker-compose -f minecraft.yml up --detach'
+alias mc='cd /home/mc-server/'
+alias dcm='docker-compose -f minecraft.yml'
